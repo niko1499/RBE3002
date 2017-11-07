8618091a6f209a6538d2d47a7f83a3faad07e59c
Nikolas Gamarra <nxgamarra@gmail.com>
2017-11-06 23:34:49 -0500

    suck on mouse bug, implementing various drive functions

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/niko/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/niko/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/niko/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/niko/catkin_ws/build"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/niko/catkin_ws/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/niko/catkin_ws/src:$ROS_PACKAGE_PATH"
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -117,6 +117,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/niko/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/niko/catkin_ws/build/nxgamarra_lab2/cmake_install.cmake")
 
 endif()
 
